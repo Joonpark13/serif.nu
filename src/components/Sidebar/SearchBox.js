8c71302af7e74b3c9f2c40caa0077b4955241ce3
Joon Park <joon.park13@gmail.com>
2018-09-30 11:44:13 -0500

    Style search box

--- src/components/Sidebar/SearchBox.js
+++ src/components/Sidebar/SearchBox.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import TextField from '@material-ui/core/TextField';
+import { withStyles } from '@material-ui/core/styles';
+
+export const styles = {
+  container: {
+    margin: 15,
+  },
+};
 
 class SearchBox extends Component {
   constructor(props) {
@@ -26,22 +33,29 @@ class SearchBox extends Component {
   }
 
   render() {
+    const { classes } = this.props;
     const { searchText } = this.state;
 
     return (
-      <TextField
-        value={searchText}
-        onChange={event => this.handleChange(event.target.value)}
-        label="Search for classes"
-        type="search"
-      />
+      <div className={classes.container}>
+        <TextField
+          value={searchText}
+          onChange={event => this.handleChange(event.target.value)}
+          label="Search for classes"
+          type="search"
+          fullWidth
+        />
+      </div>
     );
   }
 }
 
-export default SearchBox;
+export { SearchBox as UnstyledSearchBox };
+
+export default withStyles(styles)(SearchBox);
 
 SearchBox.propTypes = {
+  classes: PropTypes.objectOf(PropTypes.string).isRequired,
   handleSearchInput: PropTypes.func.isRequired,
   clearSearchResults: PropTypes.func.isRequired,
 };
