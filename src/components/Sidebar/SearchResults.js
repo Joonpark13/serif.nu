3c90005c02023c87410fb49af3f1089efe703fdd
Joon Park <jpark1@grubhub.com>
2018-10-07 15:25:50 -0500

    Search results list
    Format search results into a list using material ui list components.

--- src/components/Sidebar/SearchResults.js
+++ src/components/Sidebar/SearchResults.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 const SearchResults = ({ searchResults }) => (
-  <div>
+  <List>
     {searchResults
       && searchResults.map(course => (
-        <div key={`${course.school} ${course.subject} ${course.abbv}`}>{course.name}</div>
+        <ListItem
+          key={`${course.school} ${course.subject} ${course.abbv}`}
+          button
+        >
+          <ListItemText primary={course.name} />
+        </ListItem>
       ))
     }
-  </div>
+  </List>
 );
 
 SearchResults.propTypes = {
