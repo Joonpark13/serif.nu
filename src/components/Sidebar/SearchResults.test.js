8e833cdf9131993fd38cd475188ce030522ce536
Kevin J Lee <kevin.jihwan.lee@gmail.com>
2018-10-14 15:14:02 -0500

    Add loading indicator to search

--- src/components/Sidebar/SearchResults.test.js
+++ src/components/Sidebar/SearchResults.test.js
@@ -1,27 +1,40 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import SearchResults from './SearchResults';
+import { mockStyles } from 'util/testing';
+import { UnstyledSearchResults, styles } from './SearchResults';
 
 describe('SearchResults', () => {
+  const testResults = [{
+    abbv: '629',
+    name: 'Employment Law',
+    school: 'LAW',
+    score: 0.75,
+    subject: 'BUSCOM',
+    term: '4720',
+    type: 'course',
+  }, {
+    abbv: '631',
+    name: 'Entrepreneurship Law',
+    school: 'LAW',
+    score: 0.75,
+    subject: 'BUSCOM',
+    term: '4720',
+    type: 'course',
+  }];
+  const classes = mockStyles(styles);
+
   it('renders correctly', () => {
-    const testResults = [{
-      abbv: '629',
-      name: 'Employment Law',
-      school: 'LAW',
-      score: 0.75,
-      subject: 'BUSCOM',
-      term: '4720',
-      type: 'course',
-    }, {
-      abbv: '631',
-      name: 'Entrepreneurship Law',
-      school: 'LAW',
-      score: 0.75,
-      subject: 'BUSCOM',
-      term: '4720',
-      type: 'course',
-    }];
-    const wrapper = shallow(<SearchResults searchResults={testResults} />);
+    const wrapper = shallow(
+      <UnstyledSearchResults searchResults={testResults} isFetching={false} classes={classes} />,
+    );
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders loading correctly', () => {
+    const wrapper = shallow(
+      <UnstyledSearchResults searchResults={testResults} isFetching classes={classes} />,
+    );
 
     expect(wrapper).toMatchSnapshot();
   });
--- src/components/Sidebar/SearchResultsContainer.js
+++ src/components/Sidebar/SearchResultsContainer.js
@@ -6,9 +6,14 @@ export function searchResultsSelector(state) {
   return state.getIn(['search', 'results']);
 }
 
+export function isLoadingSelector(state) {
+  return state.getIn(['search', 'isFetching']);
+}
+
 /* istanbul ignore next */
 const mapStateToProps = state => ({
   searchResults: searchResultsSelector(state),
+  isFetching: isLoadingSelector(state),
 });
 
 const SearchResultsContainer = connect(mapStateToProps)(toJS(SearchResults));
