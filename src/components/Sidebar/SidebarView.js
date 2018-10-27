794a0f856b4b7b6acc4feb7a4d77320be86b2bb6
Kevin J Lee <kevin.jihwan.lee@gmail.com>
2018-10-27 11:46:13 -0500

    Create browse tab

--- src/components/Sidebar/SidebarView.js
+++ src/components/Sidebar/SidebarView.js
@@ -1,12 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import { withStyles } from '@material-ui/core/styles';
 import Search from './Search';
 
-function SidebarView() {
-  return (
-    <div>
-      <Search />
-    </div>
-  );
+export const styles = {
+  tabWidth: {
+    minWidth: 0,
+  },
+};
+
+class SidebarView extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: 'search',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event, value) {
+    this.setState({ value });
+  }
+
+  render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+
+    return (
+      <div>
+        <Tabs
+          value={value}
+          textColor="primary"
+          indicatorColor="primary"
+          onChange={this.handleChange}
+          fullWidth
+        >
+          <Tab value="search" label="Search" className={classes.tabWidth} />
+          <Tab value="browse" label="Browse" className={classes.tabWidth} />
+        </Tabs>
+        {value === 'search' && <Search />}
+        {value === 'browse' && <div>Todo: implement browse</div>}
+      </div>
+    );
+  }
 }
 
-export default SidebarView;
+SidebarView.propTypes = {
+  classes: PropTypes.objectOf(PropTypes.string).isRequired,
+};
+
+export { SidebarView as UnstyledSidebarView };
+export default withStyles(styles)(SidebarView);
