81636e21bc9295e15d52829cbb635db9ce0f6411
Kevin J Lee <kevin.jihwan.lee@gmail.com>
2018-11-02 10:16:59 -0500

    Format sections list

--- src/components/Sidebar/SectionSelection.js
+++ src/components/Sidebar/SectionSelection.js
@@ -1,14 +1,63 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
 
-function SectionSelection({ currentCourseName, sections, back }) {
+export const styles = {
+  sectionsRoot: {
+    padding: 16,
+  },
+  title: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  divider: {
+    marginTop: 5,
+  },
+  sectionTitle: {
+    fontWeight: 'bold',
+  },
+};
+
+function SectionSelection({
+  currentCourseName,
+  sections,
+  back,
+  classes,
+}) {
   return (
-    <div>
-      <Typography variant="headline">{currentCourseName}</Typography>
-      <Button onClick={back}>Back</Button>
-      {sections.map(section => <div key={section.id}>{`Section ${section.section}`}</div>)}
+    <div className={classes.sectionsRoot}>
+      <Typography variant="headline" className={classes.title}>
+        {currentCourseName}
+        <Button onClick={back}>Back</Button>
+      </Typography>
+      <Divider className={classes.divider} />
+      <List>
+        {sections.map(section => (
+          <ListItem
+            key={section.id}
+            button
+          >
+            <ListItemText>
+              <Typography variant="title" className={classes.sectionTitle}>{`Section ${section.section}`}</Typography>
+              <Typography>{`${section.class_mtg_info[0].meet_t}`}</Typography>
+              <Typography>{`${section.class_mtg_info[0].meet_l}`}</Typography>
+              <Typography>
+                {section.instructor.map((teacher, idx) => (
+                  `${teacher}${idx !== section.instructor.length - 1 ? ', ' : ''}`))
+                }
+              </Typography>
+            </ListItemText>
+          </ListItem>
+        ))
+        }
+      </List>
     </div>
   );
 }
@@ -17,6 +66,8 @@ SectionSelection.propTypes = {
   currentCourseName: PropTypes.string.isRequired,
   sections: PropTypes.arrayOf(PropTypes.object).isRequired,
   back: PropTypes.func.isRequired,
+  classes: PropTypes.objectOf(PropTypes.string).isRequired,
 };
 
-export default SectionSelection;
+export { SectionSelection as UnstyledSectionSelection };
+export default withStyles(styles)(SectionSelection);
--- src/components/Sidebar/SectionSelection.test.js
+++ src/components/Sidebar/SectionSelection.test.js
@@ -1,19 +1,32 @@
-import { wrapperCreator } from 'util/testing';
-import SectionSelection from './SectionSelection';
+import { mockStyles, wrapperCreator } from 'util/testing';
+import { UnstyledSectionSelection, styles } from './SectionSelection';
 
 describe('SectionSelection', () => {
+  const classes = mockStyles(styles);
+
   const defaultProps = {
     currentCourseName: 'EECS 101-0',
     sections: [{
       id: '1',
       section: 20,
+      class_mtg_info: [{
+        meet_t: 'MoWeFri 11:00AM - 11:50AM',
+        meet_l: 'Technological Institute AUD',
+      }],
+      instructor: ['Jason Hartline'],
     }, {
       id: '2',
       section: 21,
+      class_mtg_info: [{
+        meet_t: 'TuTh 11:00AM - 11:50AM',
+        meet_l: 'Technological Institute AUD',
+      }],
+      instructor: ['Ian Horswill', 'Vincent St-Amour'],
     }],
     back: () => {},
+    classes,
   };
-  const getComponent = wrapperCreator(SectionSelection, defaultProps);
+  const getComponent = wrapperCreator(UnstyledSectionSelection, defaultProps);
 
   it('renders correctly', () => {
     const wrapper = getComponent();
