92832c333c3b41b9ec5e99c24cf2d5bb837818b4
Madison <madisonfdong@gmail.com>
2019-02-16 19:23:28 -0800

    f/number-classes-indicator
    Added indicator for the number of classes that the user has selected in the cart component
    
    Changed variable name as requested by Joon.

--- src/components/Sidebar/Cart.js
+++ src/components/Sidebar/Cart.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 
-function Cart() {
-  return (<div />);
+export const styles = {
+  cartHeading: {
+    padding: '16px',
+  },
+};
+
+function Cart({ classes, sections }) {
+  const label = sections.length === 1 ? 'Class' : 'Classes';
+  return (
+    <Typography variant="h5" className={classes.cartHeading}>
+      {sections.length}
+      {' '}
+      {label}
+    </Typography>
+  );
 }
 
-export default Cart;
+Cart.propTypes = {
+  classes: PropTypes.objectOf(PropTypes.string).isRequired,
+  sections: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export { Cart as UnstyledCart };
+export default withStyles(styles)(Cart);
--- src/components/Sidebar/Cart.test.js
+++ src/components/Sidebar/Cart.test.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Cart from './Cart';
+import { mockStyles } from 'util/testing';
+import { UnstyledCart, styles } from './Cart';
 
-describe('TopBar', () => {
+describe('Cart', () => {
   it('renders correctly', () => {
-    const wrapper = shallow(<Cart />);
+    const classes = mockStyles(styles);
+    const wrapper = shallow(<UnstyledCart sections={[]} classes={classes} />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+  it('renders correctly with one section', () => {
+    const classes = mockStyles(styles);
+    const wrapper = shallow(<UnstyledCart sections={[{}]} classes={classes} />);
 
     expect(wrapper).toMatchSnapshot();
   });
