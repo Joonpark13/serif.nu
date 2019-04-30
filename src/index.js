da7b42eb8f85681485ef4a77b8758c7f17915b3d
Joon Park <jpark1@grubhub.com>
2019-04-29 21:43:02 -0500

    Add localstorage functionality

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import injectSheet from 'react-jss';
 import { createStore, compose } from 'redux';
 import { install as installReduxLoop } from 'redux-loop';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { fromJS } from 'immutable';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import App from 'components/App';
 import rootReducer from 'reducers';
+import { initialScheduleState } from 'reducers/schedule';
 import { northwesternPurple, northwesternBrightOrange } from 'util/colors';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 // https://material-ui.com/css-in-js/basics/#migration-for--material-ui-core-users
 /* eslint-disable-next-line import/newline-after-import */
@@ -46,10 +48,37 @@ if (process.env.NODE_ENV === 'production') {
 /* eslint-enable */
 }
 
+const persistedSchedule = JSON.parse(window.localStorage.getItem('schedule'));
+let persistedState;
+if (persistedSchedule) {
+  const persistedScheduleState = initialScheduleState.merge({
+    sections: persistedSchedule.sections,
+    associatedClasses: persistedSchedule.associatedClasses,
+  });
+  persistedState = fromJS({ schedule: persistedScheduleState });
+}
+
 const store = createStore(
   rootReducer,
+  persistedState,
   composeEnhancers(installReduxLoop()),
 );
+
+let previousScheduleState;
+store.subscribe(() => {
+  const state = store.getState();
+  const schedule = {
+    sections: state.getIn(['schedule', 'sections']),
+    associatedClasses: state.getIn(['schedule', 'associatedClasses']),
+  };
+  const scheduleState = JSON.stringify(schedule);
+
+  // Only set to localStorage if state we care about has changed for performance
+  if (scheduleState !== previousScheduleState) {
+    window.localStorage.setItem('schedule', scheduleState);
+    previousScheduleState = scheduleState;
+  }
+});
 
 function Index() {
   return (
