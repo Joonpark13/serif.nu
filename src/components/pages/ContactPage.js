e0b1bd3f27bce1c43bcdffd5f3337fb0a7c80dbc
ZzQ <ziqizhao15@163.com>
2019-05-19 11:08:59 -0500

    Added the reusable page title component

--- src/components/pages/ContactPage.js
+++ src/components/pages/ContactPage.js
@@ -1,27 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/styles';
-import Typography from '@material-ui/core/Typography';
-import { northwesternPurple10 } from 'util/colors';
 import { pageContainer } from './common/styles';
+import PageTitle from './common/PageTitle';
 
 export const styles = {
-  pageTitle: {
-    margin: 20,
-    padding: 5,
-    borderBottom: `2px solid ${northwesternPurple10}`,
-  },
   pageContainer,
 };
 
 function ContactPage({ classes }) {
   return (
     <div className={classes.pageContainer}>
-      <div className={classes.pageTitle}>
-        <Typography variant="h3" color="primary">
-          Contact Us
-        </Typography>
-      </div>
+      <PageTitle title="Contact Us" />
     </div>
   );
 }
--- src/components/pages/FAQPage.js
+++ src/components/pages/FAQPage.js
@@ -2,13 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/styles';
 import { pageContainer } from './common/styles';
+import PageTitle from './common/PageTitle';
 
 export const styles = {
   pageContainer,
 };
 
 function FAQPage({ classes }) {
-  return <div className={classes.pageContainer} />;
+  return (
+    <div className={classes.pageContainer}>
+      <PageTitle title="Frequently Asked Questions" />
+    </div>
+  );
 }
 
 FAQPage.propTypes = {
