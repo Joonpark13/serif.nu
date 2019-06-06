a47076c23b702d32787b1789644cff5fb9a25fe4
Kevin J Lee <kevin.jihwan.lee@gmail.com>
2019-06-05 21:36:29 -0500

    Populate the rest of the About page

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -5,6 +5,14 @@ import { UnconnectedApp } from './App';
 jest.mock('util/firebase');
 jest.mock('images/facebook-logo.png');
 jest.mock('images/hero-image.jpg');
+jest.mock('images/amy.jpg');
+jest.mock('images/helen.jpg');
+jest.mock('images/facebook-logo.png');
+jest.mock('images/joon.jpeg');
+jest.mock('images/julia.jpg');
+jest.mock('images/kevin.jpg');
+jest.mock('images/madison.jpg');
+jest.mock('images/surprised_student.svg');
 
 describe('App', () => {
   const defaultProps = {
--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { northwesternPurple10 } from 'util/colors';
 import PropTypes from 'prop-types';
-import { Typography } from '@material-ui/core';
+import { Typography, Paper } from '@material-ui/core';
 import { withStyles } from '@material-ui/styles';
 import heroImage from 'images/hero-image.jpg';
+import studentImage from 'images/surprised_student.svg';
+import joonProfile from 'images/joon.jpeg';
+import juliaProfile from 'images/julia.jpg';
+import madisonProfile from 'images/madison.jpg';
+import helenProfile from 'images/helen.jpg';
+import kevinProfile from 'images/kevin.jpg';
+import amyProfile from 'images/amy.jpg';
 
 const heroHeight = 600;
 
@@ -29,6 +37,41 @@ export const styles = {
     color: 'inherit',
     paddingTop: '20px',
   },
+  header: {
+    textAlign: 'center',
+    marginBottom: '50px',
+  },
+  blurbs: {
+    textAlign: 'center',
+    margin: '20px',
+  },
+  section: {
+    padding: '50px',
+  },
+  sectionWithColor: {
+    backgroundColor: `${northwesternPurple10}`,
+    padding: '50px',
+  },
+  multipleColumns: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+  },
+  studentImage: {
+    width: '400px',
+  },
+  studentCard: {
+    textAlign: 'center',
+    width: '300px',
+    height: '550px',
+    margin: '10px',
+  },
+  profileImage: {
+    borderRadius: '50%',
+    width: '250px',
+    margin: '10px',
+  },
 };
 
 function AboutPage({ classes }) {
@@ -38,6 +81,88 @@ function AboutPage({ classes }) {
       <div className={classes.heroText}>
         <Typography variant="h3" color="inherit">Simple. Fast. Visual.</Typography>
         <Typography variant="h5" className={classes.heroSubtitle}>Course planning for Northwestern University.</Typography>
+      </div>
+
+      <div className={classes.section}>
+        <Typography variant="h3" color="primary" className={classes.header}>Serif.nu redefines your scheduling experience.</Typography>
+
+        <div className={classes.multipleColumns}>
+          <div>
+            <Typography variant="h5" className={classes.blurbs}>No more drawing out your schedules on paper.</Typography>
+            <Typography variant="h5" className={classes.blurbs}>No more clunkly Excel spreadsheets.</Typography>
+            <Typography variant="h5" className={classes.blurbs}>Plan out your next quarter at Northwestern with ease and speed.</Typography>
+          </div>
+          <img src={studentImage} alt="Student with a laptop" className={classes.studentImage} />
+        </div>
+      </div>
+
+      <div className={classes.sectionWithColor}>
+        <Typography variant="h3" color="primary" className={classes.header}>The Team</Typography>
+
+        <Typography variant="h5" className={classes.blurbs}>Serif.nu is run by a team of previous and current Northwestern students dedicated to providing the best scheduling experience.</Typography>
+        <div className={classes.multipleColumns}>
+          <Paper className={classes.studentCard}>
+            <img src={joonProfile} alt="Joon Park" className={classes.profileImage} />
+            <Typography variant="h6">Joon Park</Typography>
+            <Typography variant="subtitle1">Creator & Team Lead</Typography>
+            <Typography variant="body2" className={classes.blurbs}>Software engineer at Grubhub, film composer in another life. Big fan of taking pictures that make me look like I&apos;m falling off of things.</Typography>
+          </Paper>
+
+          <Paper className={classes.studentCard}>
+            <img src={kevinProfile} alt="Kevin Lee" className={classes.profileImage} />
+            <Typography variant="h6">Kevin Lee</Typography>
+            <Typography variant="subtitle1">Lead Student Developer</Typography>
+            <Typography variant="body2" className={classes.blurbs}>Senior studying computer science...and nothing else! Strong enthusiast of deep dish pizza, the color turquoise, and Korean music. </Typography>
+          </Paper>
+
+          <Paper className={classes.studentCard}>
+            <img src={madisonProfile} alt="Madison Dong" className={classes.profileImage} />
+            <Typography variant="h6">Madison Dong</Typography>
+            <Typography variant="subtitle1">Student Developer</Typography>
+            <Typography variant="body2" className={classes.blurbs}>
+              Madison Dong is a (soon-to-be) junior studying journalism,
+              computer science, and design,
+              so she thinks art and tech go well together.
+              Her three pillars of life are: adventures with friends,
+              7-Eleven Big Bite hot dogs, and Frank Ocean.
+              Send her funny emails and/or articles: madisonfdong@gmail.com
+            </Typography>
+          </Paper>
+
+          <Paper className={classes.studentCard}>
+            <img src={juliaProfile} alt="Julia Tournant" className={classes.profileImage} />
+            <Typography variant="h6">Julia Tournant</Typography>
+            <Typography variant="subtitle1">Student Developer</Typography>
+            <Typography variant="body2" className={classes.blurbs}>
+              Hi! I’m Julia. I’m French, currently 19 years old, and will graduate in June 2021.
+              I study computer science and enjoy learning foreign languages.
+              I’m also a member of the varsity golf team here at NU!
+              You can reach out to me at juliatournant2021@u.northwestern.edu.
+            </Typography>
+          </Paper>
+
+          <Paper className={classes.studentCard}>
+            <img src={helenProfile} alt="Helen Zhao" className={classes.profileImage} />
+            <Typography variant="h6">Helen Zhao</Typography>
+            <Typography variant="subtitle1">Student Developer</Typography>
+            <Typography variant="body2" className={classes.blurbs}>
+              I am Helen, a Northwestern sophomore.
+              Love cooking, love taekwondo and especially love watching dogs sneeze
+              <span role="img" aria-label="sneezing face"> 🤧</span>
+            </Typography>
+          </Paper>
+
+          <Paper className={classes.studentCard}>
+            <img src={amyProfile} alt="Amy Chen" className={classes.profileImage} />
+            <Typography variant="h6">Amy Chen</Typography>
+            <Typography variant="subtitle1">Student Developer</Typography>
+            <Typography variant="body2" className={classes.blurbs}>
+              My name is Amy and I&apos;m majoring in Statistics, Economics, and MMSS.
+              I&apos;m pursuing a career in software engineering.
+              Outside of school, I love tennis and photography!
+            </Typography>
+          </Paper>
+        </div>
       </div>
     </div>
   );
