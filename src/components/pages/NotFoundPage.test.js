123fa47496f5f9a3ef88d0ff4e381bc280af4ec4
Kevin J Lee <kevin.jihwan.lee@gmail.com>
2019-07-01 19:20:44 -0700

    404 Page

---
+++ src/components/pages/NotFoundPage.test.js
@@ -0,0 +1,12 @@
+// import { wrapperCreator } from 'util/testing';
+// import { NotFoundPage, useStyles } from './NotFoundPage';
+
+// describe('FAQPage', () => {
+//   const getWrapper = wrapperCreator(NotFoundPage, undefined, useStyles);
+
+//   it('should render correctly', () => {
+//     const wrapper = getWrapper();
+
+//     expect(wrapper.get(0)).toMatchSnapshot();
+//   });
+// });
